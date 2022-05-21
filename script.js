505638293e15dab19ef0db6494e58876c56aa0b6
Pete Fowler <fowler.pete@gmail.com>
2022-05-21 11:50:50 -0600

    Display all numbers

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ const nine = document.getElementById('nine');
 const zero = document.getElementById('zero');
 const display = document.getElementById('display');
 
-let displayValue = [];
+let displayValue = '';
 
 one.addEventListener('click', () => {changeDisplay(1);});
+two.addEventListener('click', () => {changeDisplay(2);});
+three.addEventListener('click', () => {changeDisplay(3);});
+four.addEventListener('click', () => {changeDisplay(4);});
+five.addEventListener('click', () => {changeDisplay(5);});
+six.addEventListener('click', () => {changeDisplay(6);});
+seven.addEventListener('click', () => {changeDisplay(7);});
+eight.addEventListener('click', () => {changeDisplay(8);});
+nine.addEventListener('click', () => {changeDisplay(9);});
+zero.addEventListener('click', () => {changeDisplay(0);});
 
 function add(a, b) {
     return a + b;
