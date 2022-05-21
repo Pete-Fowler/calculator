87664f778572b9896d1dc23c707650fd0474977f
Pete Fowler <fowler.pete@gmail.com>
2022-05-21 11:15:50 -0600

    Simplify display html and begin js display()

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const displaywindow = document.getElementById('display-numbers');
+
+let 
+
+one.addEventListener('click', display(one));
+
 function add(a, b) {
     return a + b;
 }
@@ -16,4 +32,8 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     return operator(a, b);
+}
+
+function display(num) {
+
 }
